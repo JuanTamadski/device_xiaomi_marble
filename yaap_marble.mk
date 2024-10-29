249efaed4f9813cc0ec7ad31a5cd0f3b368b1432
JuanTamadski <76487328+JuanTamadski@users.noreply.github.com>
2024-10-29 10:40:33 +0800

    adapt to yaap source

--- yaap_marble.mk
+++ yaap_marble.mk
@@ -8,16 +8,18 @@
 $(call inherit-product, device/xiaomi/marble/device.mk)
 
 # Inherit from common lineage configuration
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
 
-EVO_BUILD_TYPE := Official
+
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_BUILD_APERTURE_CAMERA := false
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_marble
+PRODUCT_NAME := yaap_marble
 PRODUCT_DEVICE := marble
 PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := marble
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := POCO/marble_global/marble:14/UKQ1.230804.001/V816.0.9.0.UMRMIXM:user/release-keys
